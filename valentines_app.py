7dc6b2fdda95ba8fe65aea18cc03c5c48d544cfd
Sebastian Bernal-Orta <sebastbernal2@gmail.com>
2026-02-02 03:08:29 -0500

    Adjust height parameter in components.html call

--- valentines_app.py
+++ valentines_app.py
@@ -14,4 +14,4 @@ html_path = current_dir / "valentines.html"
 with open(html_path, "r", encoding="utf-8") as f:
     html_content = f.read()
 
-components.html(html_content, height=650, scrolling=False)
+components.html(html_content, height=550, scrolling=False)
